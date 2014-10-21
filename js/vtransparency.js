9967f2f4bb4a74e960a0ccef5ed43000ee70c923
mlofgren <matt.lofgren@state.vt.us>
2014-10-21 16:26:59 -0400

    added some comments. + scroll to opened item

--- js/vtransparency.js
+++ js/vtransparency.js
@@ -20,17 +20,20 @@ $( document ).ready(function() {
 
 	//when a box is clicked
 	$( "#contentFeatures > div" ).click(function() {
-		if( $("#slider > div").is(':animated') ) {console.log("blocked click"); return;}
+	
+		// prevent double clicking an item while it is animating.
+		if( $("#slider > div").is(':animated') ) {return;}
 				
-		
+		// store value of 'this'
 		var clickedItem = this;
+		
+		
 		if (isOpen && this.id == openedItem){
 			$("#slider > div").slideToggle( "fast", function() {
 				isOpen = false;
 				$("#slider > div").css("display","none");
 				$("#slider > div").appendTo("#sliderContent");	
 				$("#slider").remove();
-				console.log("done closeing callback");
 			});	
 			return;
 		}
@@ -40,71 +43,50 @@ $( document ).ready(function() {
 				$("#slider > div").css("display","none");
 				$("#slider > div").appendTo("#sliderContent");	
 				$("#slider").remove();
-				
-				
-				
-				console.log("moving on");
 			
-				openedItem = clickedItem.id;
-				var node = $("#open" + clickedItem.id)[0];
-				var spot = findSpot(clickedItem.id);
-				var elem = clickedItem;
-				
-				for (i = 0; i < spot; i++)
-				{	
-					if ($("#" + elem.id).next().length) {
-					  //has next 
-					  elem = $("#" + elem.id).next()[0];
-					} else {
-					  //last elem
-					  i = 999;
-					}			
-				}
-				
-				$("<div id='slider' class='col-md-3 col-sm-6 hero-feature'></div>").insertAfter(elem);
-				console.log(elem);
-				console.log(elemsPerLine);
-				$("#" + node.id).appendTo("#slider");		
-				$("#slider > div").slideToggle( "fast", function() {
-					isOpen = true;
-				});		
+				newSlider(clickedItem);
 			});	
 		}
-		else{
-			console.log("moving on");
-			
-			openedItem = this.id;
-			var node = $("#open" + this.id)[0];
-			var spot = findSpot(this.id);
-			var elem = this;
-			
-			for (i = 0; i < spot; i++)
-			{	
-				if ($("#" + elem.id).next().length) {
-				  //has next 
-				  elem = $("#" + elem.id).next()[0];
-				} else {
-				  //last elem
-				  i = 999;
-				}			
-			}
-			
-			$("<div id='slider' class='col-md-3 col-sm-6 hero-feature'></div>").insertAfter(elem);
-			console.log(elem);
-			console.log(elemsPerLine);
-			$("#" + node.id).appendTo("#slider");		
-			$("#slider > div").slideToggle( "fast", function() {
-				isOpen = true;
-			});
+		else{		
+			newSlider(clickedItem);
 		}
 	}).dblclick(function(e){
-		//Prevent double-clicks.	 	 
+		//prevent double-clicks.	 	 
 		e.stopPropagation();
 		e.preventDefault();
 		return false;
 	});;
+	
+	function newSlider(clickedItem){
+		openedItem = clickedItem.id;
+		var node = $("#open" + clickedItem.id)[0];
+		var spot = findSpot(clickedItem.id);
+		var elem = clickedItem;
+		
+		//find the last box in the row.
+		for (i = 0; i < spot; i++)
+		{	
+			if ($("#" + elem.id).next().length) {
+			  //has next 
+			  elem = $("#" + elem.id).next()[0];
+			} else {
+			  //last elem
+			  i = 999;
+			}			
+		}
+		
+		
+		$("<div id='slider' class='col-md-3 col-sm-6 hero-feature'></div>").insertAfter(elem);
+		$("#" + node.id).appendTo("#slider");		
+		$("#slider > div").slideToggle( "fast", function() {
+			isOpen = true;
+		});	
+		$('html, body').animate({
+			scrollTop: $("#" + openedItem).offset().top - 60
+		}, 1000);
+	}
 
-	// calculate how many items are displayed in one line based on 
+	// calculate how many items are displayed in one line based on media queries.
 	function returnElemsPerLine(){
 		if ($("#temp").css("width") == "3px" ){
 			return 5;
@@ -117,6 +99,9 @@ $( document ).ready(function() {
 		}	
 	}
 	
+	// We want to slider box to show up on the next line. 
+	// We need to return how many boxes are left till the end of the line
+	// so we can insert this as the next sibling.
 	function findSpot(num){
 		if (elemsPerLine == 5){
 			return (5 - (num%5))		
